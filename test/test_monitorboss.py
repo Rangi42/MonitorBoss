04771b8a431851be782a4711091b37bf91359aa1
Jesse Weiman <jesse.weiman@storable.com>
2024-04-25 22:20:03 -0400

    starting out mb testing

--- test/test_monitorboss.py
+++ test/test_monitorboss.py
@@ -1,5 +1,29 @@
 import pytest
+
+from monitorboss import MonitorBossError
 from pyddc import VCP
+from monitorboss.impl import list_monitors, __get_monitor
+
+
+def test_list_monitors():
+    vcps = list_monitors()
+    assert len(vcps) == 3
+    for vcp in vcps:
+        assert isinstance(vcp, VCP)
+
+
+def test_get_monitor():
+    with pytest.raises(MonitorBossError):
+        __get_monitor(4)
+
+    # TODO: is it desirable behavior that negative indexes work?
+    # with pytest.raises(MonitorBossError):
+    #     print(__get_monitor(-1))
+
+    assert isinstance(__get_monitor(0), VCP)
 
 
 
+# TODO: do we need to actually test any of these functions?
+# get_vcp_capabilities
+# get_attribute
